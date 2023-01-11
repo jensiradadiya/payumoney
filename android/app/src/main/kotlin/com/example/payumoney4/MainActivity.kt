4afad6206183b23859f3ab6f7664a8de73196c9c
jensiradadiya <96307370+jensiradadiya@users.noreply.github.com>
2023-01-11 17:11:24 +0530

    a
    a

---
+++ android/app/src/main/kotlin/com/example/payumoney4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.payumoney4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<payumoney_pro_unofficial/PayumoneyProUnofficialPlugin.h>)
+#import <payumoney_pro_unofficial/PayumoneyProUnofficialPlugin.h>
+#else
+@import payumoney_pro_unofficial;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [PayumoneyProUnofficialPlugin registerWithRegistrar:[registry registrarForPlugin:@"PayumoneyProUnofficialPlugin"]];
+}
+
+@end
